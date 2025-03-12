48708bc9550bcdc44372d13a7ce358f2a2d0f9c0
TortleWortle <mail@tortle.tech>
2025-03-12 09:11:36 +0100

    make *inertia.Ctx compatible with http.ResponseWriter

--- pkg/inertia/handler.go
+++ pkg/inertia/handler.go
@@ -1,12 +1,15 @@
 package inertia
 
 import (
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
 
 	"github.com/tortlewortle/yaigo/internal/errflash"
 )
+
+var ErrDirtyRender = errors.New("ResponseWriter has already been written to")
 
 var DefaultErrHandler = func(w http.ResponseWriter, r *http.Request, err error) {
 	slog.Error("handler error", slog.String("err", err.Error()))
@@ -32,10 +35,36 @@ func Handler(handlerFunc HandlerFunc) http.HandlerFunc {
 type Ctx struct {
 	request *http.Request
 	writer  http.ResponseWriter
+
+	dirty bool // keep track on wether this request has been manually written to
+}
+
+// compat for http.ResponseWriter
+var _ http.ResponseWriter = &Ctx{}
+
+// Header implements http.ResponseWriter.
+func (c *Ctx) Header() http.Header {
+	return c.writer.Header()
+}
+
+// Write implements http.ResponseWriter.
+func (c *Ctx) Write(b []byte) (int, error) {
+	c.dirty = true
+
+	return c.writer.Write(b)
+}
+
+// WriteHeader implements http.ResponseWriter. DO NOT USE TO SET THE STATUS CODE
+func (c *Ctx) WriteHeader(statusCode int) {
+	c.dirty = true
+	c.writer.WriteHeader(statusCode)
 }
 
 // Render queues the component for rendering after the handler finishes
 func (c *Ctx) Render(page string, props Props) error {
+	if c.dirty {
+		return ErrDirtyRender
+	}
 	server, err := getServer(c.request)
 	if err != nil {
 		return err
@@ -47,7 +76,9 @@ func (c *Ctx) Render(page string, props Props) error {
 	return server.Render(req, c.writer, c.request, page, props)
 }
 
+// Error renders the Error component and will print out the cause in devmode (not implemented yet)
 func (c *Ctx) Error(cause error, status int) error {
+	// todo: check devmode, render pretty component instead of raw error if not devmode
 	fmt.Fprintf(c.writer, "error: %v", cause)
 
 	return nil
