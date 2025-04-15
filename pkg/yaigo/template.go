58d717e83c14a5f9370ec9f6692f076ece821953
TortleWortle <mail@tortle.tech>
2025-04-15 22:21:42 +0200

    can't fix stupid but I'm sure as hell trying

--- pkg/yaigo/template.go
+++ pkg/yaigo/template.go
@@ -43,7 +43,7 @@ func generateRootTemplate(tfn func(*template.Template) (*template.Template, erro
 				return "", err
 			}
 			for _, sheetUrl := range item.Css {
-				tb.WriteString(fmt.Sprintf("<link rel=\"preload\" href=\"/%s\"> as=\"style\"/>\n", sheetUrl))
+				tb.WriteString(fmt.Sprintf("<link rel=\"preload\" href=\"/%s\" as=\"style\"/>\n", sheetUrl))
 			}
 			tb.WriteString("\n")
 			for _, sheetUrl := range item.Css {
